5b6482cba3f98b49e7fb573357a9e87bdfd27983
llyogesh123 <yogeshwaranjith2004@gmail.com>
2025-02-25 00:30:05 +0530

    container included

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: "*", credentials: true }));
@@ -27,10 +29,13 @@ app.use("/api/buses", busRoutes);
 app.use("/api/bookings", bookingRoutes);
 
 // Database Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
